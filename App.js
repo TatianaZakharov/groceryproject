2a04db2e7b20bd568ba201f4928ac5a6b09b897c
Tatiana Zakharov <tanushka.z2010@gmail.com>
2020-10-20 18:16:16 -0400

    Initial comit

---
+++ App.js
@@ -0,0 +1,274 @@
+import React, { Component } from 'react';
+import { AppRegistry, Text, TextInput, updateState, View, TouchableHighlight, Dimensions, StyleSheet } from 'react-native';
+import Constants from 'expo-constants';
+
+let deviceHeight = Dimensions.get('window').height;
+let deviceWidth = Dimensions.get('window').width;
+
+export default class App extends Component {
+
+    // Initially, Welcome page is displayed
+    state = {
+        welcomePageDisplay: 'block',
+        infoPageDisplay: 'none',
+        meat: '', 
+        snack: '', 
+        beverage: '',
+        veg: '',
+    }
+    clearState = () => {
+      this.setState({ 
+          meat: '',
+          snack:'',
+          beverage: '',
+          veg: '',
+      })
+  }
+
+    // When Welcome Page button is pressed, hide Info page and show Welcome page
+    handleWelcomePagePress = () => this.setState(state => ({
+        welcomePageDisplay: 'block',
+        infoPageDisplay: 'none',
+    }));
+    
+    // When Info Page button is pressed, hide Welcome page and show Info page
+    handleInfoPagePress = () => this.setState(state => ({
+        welcomePageDisplay: 'none',
+        infoPageDisplay: 'block',
+    }));
+
+    
+    render() {
+        return (
+            <View style={styles.container}>
+                
+                {/*Welcome page screen layout here*/}
+                <View style={{ display: this.state.welcomePageDisplay }}>
+                    <View style={styles.contentContainer}>
+                        <Text style={styles.title1}>
+                            Welcome to Shop App!
+                        </Text>
+                        <Text style = {styles.title1}>
+                          Grocery List Organizer
+                        </Text>
+                    </View>
+                </View>
+                
+                {/*Info page screen layout here*/}
+                <View style={{ display: this.state.infoPageDisplay }}>
+                    < View style={styles.contentContainer}>
+                        <Text style={styles.paragraph}>
+                            These Are Your Groceries! 
+                        </Text>
+
+                        <View style= {styles.cancelBox}>
+                          <Text style={styles.cancel}
+                         onPress = {this.clearState}
+                          >
+                            clear 
+                           </Text>
+                        </View>
+
+                        
+                        <View style = {styles.groceries}>
+                          <Text style = {styles.food}>
+                            Meat
+                          </Text>
+                          <TextInput style={styles.meat}
+                            multiline
+                            numberOfLines={8}
+                            onChangeText={(meat) => this.setState({meat})}
+                            value={this.state.meat}
+                            />
+                        </View>
+                        
+
+
+                        <View style = {styles.groceries2}>
+                          <Text style = {styles.food}>
+                            Snack
+                          </Text>
+                          <TextInput style={styles.snack}
+                            multiline
+                            numberOfLines={8}
+                            onChangeText={(snack) => this.setState({snack})}
+                            value={this.state.snack}
+                            />
+                        </View>
+
+
+
+                        <View style = {styles.groceries3}>
+                          <Text style = {styles.food}>
+                            Beverage
+                          </Text>
+                          <TextInput style={styles.beverage}
+                            multiline
+                            numberOfLines={8}
+                            onChangeText={(beverage) => this.setState({beverage})}
+                            value={this.state.beverage}
+                            />
+                        </View>
+
+
+
+                        <View style = {styles.groceries4}>
+                          <Text style = {styles.food}>
+                            Vegetable/Fruit 
+                          </Text>
+                          <TextInput style={styles.veg}
+                            multiline
+                            numberOfLines={8}
+                            onChangeText={(veg) => this.setState({veg})}
+                            value={this.state.veg}
+                            />
+                        </View>
+                    </View>
+                </View>
+                
+                {/*Bottom nav bar shows on each screen*/}
+                <View style={styles.navbarContainer}>
+                    <TouchableHighlight style={styles.navButton}
+                    onPress={this.handleWelcomePagePress}
+                    >
+                        <Text style={styles.navButtonText}>
+                            Home Page 
+                        </Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight style={styles.navButton}
+                    onPress={this.handleInfoPagePress}
+                    >
+                        <Text style={styles.navButtonText}>
+                            Make Grocery List 
+                        </Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+      );
+   }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: deviceHeight,
+        width: deviceWidth,
+    },
+    contentContainer: {
+        height: 5*(deviceHeight/6),
+        width: deviceWidth,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFDCCC',
+    },
+    title1: {
+        fontSize: deviceHeight/10,
+        textAlign: 'center',
+        fontWeight: 'bold',
+        color: '#FB6376',
+    },
+    paragraph: {
+        fontSize: deviceHeight/20,
+        textAlign: 'center',
+        color: '#33B4FF',
+        fontWeight: 'bold',
+    },
+    navbarContainer: {
+        height: deviceHeight/6,
+        width: deviceWidth,
+        backgroundColor: '#FFDCCC',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderTopWidth: 5,
+        borderColor: 'black',
+    },
+    navButton: {
+        height: deviceHeight/14,
+        width: deviceWidth/4,
+        backgroundColor: '#FB6376',
+        borderColor: 'black',
+        borderWidth: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 20,
+    },
+    navButtonText: {
+        fontSize: deviceHeight/40,
+        textAlign: 'center',
+        fontWeight: 'bold', 
+        color: '#90D7FF',
+    },
+    groceries: {
+      width: deviceWidth,
+      height: deviceHeight/5, 
+      backgroundColor: '#FFDCCC', 
+      borderColor: '#FB6376',
+      borderWidth: 7,
+    },
+    groceries2: {
+      width: deviceWidth,
+      height: deviceHeight/5, 
+      backgroundColor: '#FFDCCC', 
+      borderColor: '#5D2A42',
+      borderWidth: 7,
+    },
+    groceries3: {
+      width: deviceWidth,
+      height: deviceHeight/5, 
+      backgroundColor: '#FFDCCC', 
+      borderColor: '#FB6376',
+      borderWidth: 7,
+    },
+    groceries4: {
+      width: deviceWidth,
+      height: deviceHeight/5, 
+      backgroundColor: '#FFDCCC', 
+      borderColor: '#5D2A42',
+      borderWidth: 7,
+    },
+    food: {
+      fontSize: 30,
+      textAlign: 'center',
+      fontWeight: 'bold',
+      color: '#33B4FF',
+    },
+    meat: {
+      height: 150,
+      borderWidth: 3,
+      color: '#90D7FF',
+      fontSize: 20,
+      textAlign: 'center',
+  }, 
+  snack: {
+    height: 150,
+    borderWidth: 3,
+    color: '#90D7FF',
+    fontSize: 20,
+    textAlign: 'center',
+},
+beverage: {
+  height: 150,
+  borderWidth: 3,
+  color: '#90D7FF',
+  fontSize: 20,
+  textAlign: 'center',
+}, 
+veg: {
+  height: 150,
+  borderWidth: 3,
+  color: '#90D7FF',
+  fontSize: 20,
+  textAlign: 'center',
+},
+cancelBox: {
+  width: 40,
+  height: 20, 
+  backgroundColor: 'white',
+},
+cancel: {
+  fontSize: 15,
+  color: 'black',
+  fontWeight: 'bold',
+}
+
+});
