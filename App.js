1f023a03494b46b1101165d661d86c63465e0e58
Tatiana Zakharov <tanushka.z2010@gmail.com>
2020-12-01 19:31:04 -0500

    Change home item

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import { ImageBackground, Text, View, style} from 'react-native';
+import { ImageBackground, Text, View, style } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { mdiHomeRoof } from '@mdi/js';
 import GroceryList from './GroceryList';
 import ListOfLists from './ListOfLists';
 
@@ -11,62 +12,66 @@ import ListOfLists from './ListOfLists';
 function HomeScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      
-      <ImageBackground source={{ uri: 'https://wizardlyshoe4.s-ul.eu/sRPafkZt.jpg' }} style={{width: '100%', height: '100%'}}>
-    
-         <Text>Home Screen !</Text>
+
+      <ImageBackground source={{ uri: 'https://wizardlyshoe4.s-ul.eu/sRPafkZt.jpg' }} style={{ width: '100%', height: '100%' }}>
+
+        <Text>Home Screen !</Text>
 
       </ImageBackground>
-    
+
     </View>
   );
 }
- 
 
- 
+
+
 const Tab = createBottomTabNavigator();
- 
+
 function AllTabs() {
   return (
     <Tab.Navigator>
- 
-      <Tab.Screen 
-      name="Home" 
-      component={HomeScreen}
-      options={{
-        tabBarLabel: 'Home',
-        tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-        ) }}
-        
-       />
- 
-      <Tab.Screen 
-      name="Groceries" 
-      component={GroceryList}
-      options={{
-        tabBarLabel: 'Groceries',
-        tabBarIcon: ({ color, size }) => (
+
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home-outline" color={color} size={size} />
+            
+          )
+        }}
+
+      />
+
+      <Tab.Screen
+        name="Groceries"
+        component={GroceryList}
+        options={{
+          tabBarLabel: 'Groceries',
+          tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="basket-fill" color={color} size={size} />
-        ) }}
-       />
+          )
+        }}
+      />
 
-      <Tab.Screen 
-      name="Lists" 
-      component={ListOfLists}
-      options={{
-        tabBarLabel: 'Groceries',
-        tabBarIcon: ({ color, size }) => (
+      <Tab.Screen
+        name="Lists"
+        component={ListOfLists}
+        options={{
+          tabBarLabel: 'Groceries',
+          tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="basket-fill" color={color} size={size} />
-        ) }}
-       />
+          )
+        }}
+      />
 
 
- 
+
     </Tab.Navigator>
   );
 }
- 
+
 export default function App() {
   return (
     <NavigationContainer>
